be67d96dad890750905a19a67bd1f48a9f5f52af
Nikolas Laus <nikolas.laus@protonmail.com>
2020-03-18 18:04:32 +0200

    Update Program.cs
    Added first function.

--- Program.cs
+++ Program.cs
@@ -14,17 +14,20 @@ namespace SnakeGame
             int y1 = 1;
             char sym1 = '*';
 
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(sym1);
+            Draw(x1, y1, sym1);
 
             int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
 
-            Console.SetCursorPosition(x2, y2);
-            Console.Write(sym2);
+            Draw(x2, y2, sym2);
 
             Console.ReadLine();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
