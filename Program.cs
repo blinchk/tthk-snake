a36fa44543983667eb4cebe0ba1df4493056cda0
Nikolas Laus <nikolas.laus@protonmail.com>
2020-03-18 18:00:41 +0200

    Update Program.cs
    Added dot and bars to the console.

--- Program.cs
+++ Program.cs
@@ -10,8 +10,21 @@ namespace SnakeGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int x1 = 1;
+            int y1 = 1;
+            char sym1 = '*';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Console.SetCursorPosition(x2, y2);
+            Console.Write(sym2);
+
+            Console.ReadLine();
         }
     }
 }
