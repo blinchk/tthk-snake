ca32b7e6d7334d218bc273d0ba2bf4150628a8b1
Nikolas Laus <bchik3@gmail.com>
2020-03-20 00:40:48 +0200

    New objects from tutorial Ep. 9

--- Program.cs
+++ Program.cs
@@ -10,11 +10,23 @@ namespace SnakeGame
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(80, 25);
+
+
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
             Console.ReadLine();
         }
