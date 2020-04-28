354563c9e44d0100a515ec733ec7755a87afc68a
Nikolas Laus <bchik3@gmail.com>
2020-04-29 01:43:54 +0300

    Added class Messages for show "Game over", if player loses. "Game over" was absolutely deleted from Main methods.

---
+++ Messages.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    class Messages
+    {
+		public void WriteGameOver(string name, int score)
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+			WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+			WriteText("Для перезапуска нажмите Enter", xOffset, yOffset++);
+			yOffset++;
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			WriteText("Пытался: Nikolas Laus", xOffset + 2, yOffset++);
+			WriteText("Группа: TARpv19", xOffset + 2, yOffset++);
+			Console.ForegroundColor = ConsoleColor.White;
+			WriteText(name + ", спасибо за игру!", xOffset++, yOffset++);
+			WriteText("Ваш конечный счёт: " + score, xOffset + 2, yOffset++);
+			Console.ForegroundColor = ConsoleColor.Red;
+			WriteText("============================", xOffset, yOffset++);
+			Console.WriteLine(score);
+			using (var file = new StreamWriter("score.txt", true))
+			{
+				file.WriteLine("Name: " + name + " | Score: " + score);
+				file.Close();
+			}
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -76,45 +76,13 @@ namespace SnakeGame
 					snake.HandleKey(key.Key);
 				}
 			}
-			WriteGameOver(name, snake.score);
+			Messages gameover = new Messages();
+			gameover.WriteGameOver(name, snake.score);
 			sound.Stop();
 			pointsound.Play("lose");
 			ConsoleKeyInfo _key = Console.ReadKey();
 			if (_key.Key == ConsoleKey.Enter)
 				Application.Restart();
-		}
-
-
-		static void WriteGameOver(string name, int score)
-		{
-			int xOffset = 25;
-			int yOffset = 8;
-			Console.ForegroundColor = ConsoleColor.Red;
-			Console.SetCursorPosition(xOffset, yOffset++);
-			WriteText("============================", xOffset, yOffset++);
-			WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
-			WriteText("Для перезапуска нажмите Enter", xOffset, yOffset++);
-			yOffset++;
-			Console.ForegroundColor = ConsoleColor.Yellow;
-			WriteText("Пытался: Nikolas Laus", xOffset+2, yOffset++);
-			WriteText("Группа: TARpv19", xOffset + 2, yOffset++);
-			Console.ForegroundColor = ConsoleColor.White;
-			WriteText(name + ", спасибо за игру!", xOffset++, yOffset++);
-			WriteText("Ваш конечный счёт: " + score, xOffset + 2, yOffset++);
-			Console.ForegroundColor = ConsoleColor.Red;
-			WriteText("============================", xOffset, yOffset++);
-			Console.WriteLine(score);
-			using( var file = new StreamWriter("score.txt", true) )
-			{
-				file.WriteLine("Name: " + name + " | Score: " + score);
-				file.Close();
-			}
-		}
-
-		static void WriteText(String text, int xOffset, int yOffset)
-		{
-			Console.SetCursorPosition(xOffset, yOffset);
-			Console.WriteLine(text);
 		}
 
 	}
